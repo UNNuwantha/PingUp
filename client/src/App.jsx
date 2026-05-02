d47b3ef80c6a292747856958747c7fcab7fdc779
UNNuwantha <www.nadun50@gmail.com>
2026-05-02 07:00:16 +0530

    Created the folder structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Feed from './pages/Feed'
+import Messages from './pages/Messages'
+import ChatBox from './pages/ChatBox'
+import Connetions from './pages/Connetions'
+import Discover from './pages/Discover'
+import Profile from './pages/Profile'
+import CreatePost from './pages/CreatePost'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Login />}>
+        <Route index element={<Feed />} />
+        <Route path='messages' element={<Messages />} />
+        <Route path='messages/:userId' element={<ChatBox />} />
+        <Route path='connections' element={<Connetions />} />
+        <Route path='discover' element={<Discover />} />
+        <Route path='profile' element={<Profile />} />
+        <Route path='profile/:profileId' element={<Profile />} />
+        <Route path='create-post' element={<CreatePost />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
